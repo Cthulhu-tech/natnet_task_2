b709669b097f81afd4a362f505f4f6bbf3568767
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-03-20 05:50:51 +0200

    end

---
+++ public/serviceworker.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = "version-1";
+const urlsToCache = ["index.html", "offline.html"];
+
+const self = this;
+
+self.addEventListener('install', (event) => {
+
+});
+
+self.addEventListener('install', (event) => {
+
+});
+
+self.addEventListener('install', (event) => {
+
+});
